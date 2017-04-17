fd2b1d0bbc285d8349b4583feceada1d16783c20
Seth Gunnells <sethgunnells@gmail.com>
2017-04-17 14:44:23 -0500

    Add "create" command shell

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ var program = require('commander');
 program
   .version('0.0.1');
 
+program
+  .command('create <taskName>')
+  .description('Create a new task')
+  .action(createNewTask);
+
 program.parse(process.argv);
 
 tooFewArguments();
 
 ////////////////////////////////////////////////////////////////////////////////
+
+function createNewTask(taskName) {
+  console.log('Create New Task Called');
+  console.log(`New Task Name: ${JSON.stringify(taskName)}`);
+}
 
 function tooFewArguments() {
   console.log('You must specify an action from the following list: create');
