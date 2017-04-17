22b1342663d2a8d326c73589d50a4640cac7e1ff
Seth Gunnells <sethgunnells@gmail.com>
2017-04-17 14:33:17 -0500

    import commander library

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
+var program = require('commander');
 
-var args = process.argv.slice(2);
-console.log(args.join(' '));
+program
+  .version('0.0.1');
+
+program.parse(process.argv);
+
+tooFewArguments();
+
+////////////////////////////////////////////////////////////////////////////////
+
+function tooFewArguments() {
+  console.log('You must specify an action from the following list: create');
+  process.exit(1);
+}
