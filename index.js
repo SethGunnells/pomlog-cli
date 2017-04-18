e78b226e0aea5c26601b964f8eed8693fef980c8
Seth Gunnells <sethgunnells@gmail.com>
2017-04-18 01:33:55 -0500

    Add Firebase

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 #!/usr/bin/env node
 var program = require('commander');
+var firebase = require('firebase');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBnq04uewXYu4oOiYNZ7KvsJlRMi6keFXQ",
+  authDomain: "trackadoro.firebaseapp.com",
+  databaseURL: "https://trackadoro.firebaseio.com",
+  projectId: "trackadoro",
+  storageBucket: "trackadoro.appspot.com",
+  messagingSenderId: "139300037615"
+};
+firebase.initializeApp(config);
+
+var db = firebase.database();
 
 program
   .version('0.0.1');
@@ -16,4 +30,10 @@ program.parse(process.argv);
 function createNewTask(taskName) {
   console.log('Create New Task Called');
   console.log(`New Task Name: ${taskName}`);
+  db.ref('tasks').push({ name: taskName })
+    .then(close);
+}
+
+function close() {
+  firebase.app().delete();
 }
