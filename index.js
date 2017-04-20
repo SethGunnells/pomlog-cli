b504bad4ae74f9df27b17870ff37fe266484bac3
Seth Gunnells <seth.gunnells@decisionsource.com>
2017-04-20 13:42:50 -0500

    Remove firebase

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 #!/usr/bin/env node
 var program = require('commander');
-var firebase = require('firebase');
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBnq04uewXYu4oOiYNZ7KvsJlRMi6keFXQ",
-  authDomain: "trackadoro.firebaseapp.com",
-  databaseURL: "https://trackadoro.firebaseio.com",
-  projectId: "trackadoro",
-  storageBucket: "trackadoro.appspot.com",
-  messagingSenderId: "139300037615"
-};
-firebase.initializeApp(config);
-
-var db = firebase.database();
 
 program
   .version('0.0.1');
 
 program
-  .command('create <taskName>')
+  .command('create [type] <taskName>')
   .description('Create a new task')
   .action(createNewTask);
 
@@ -27,13 +13,8 @@ program.parse(process.argv);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-function createNewTask(taskName) {
+function createNewTask(type, taskName) {
   console.log('Create New Task Called');
+  console.log(`New Level Type: ${type}`);
   console.log(`New Task Name: ${taskName}`);
-  db.ref('tasks').push({ name: taskName })
-    .then(close);
-}
-
-function close() {
-  firebase.app().delete();
 }
